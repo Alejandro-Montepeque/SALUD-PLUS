702360b8beb520cfd46fad6c485c4ad90b2ad480
alejandro-montepeque <jonathanmontepeque@gmail.com>
2024-10-02 17:53:00 -0600

    adding: new view for reccovery account

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Login from './views/Login.vue';
 import Register from './views/Register.vue'
+import Recovery from './views/RecoveryPassword.vue'
 
 const routes = [
     {
@@ -13,6 +14,11 @@ const routes = [
         path: '/register',
         name: 'Register',
         component: Register
+    },
+    {
+        path: '/recovery-password',
+        name: 'Recovery',
+        component: Recovery
     }
 ]
 
